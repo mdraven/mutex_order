6fad267b410d934f8c2ee9d7334de81116c8c185
Ramil Iljasov <dravenshadowkeeper@gmail.com>
2022-11-07 15:25:13 +0500

    ddddddd

--- main.cpp
+++ main.cpp
@@ -20,35 +20,60 @@
 #include <functional>
 
 template<class Tag>
-class named_mutex {};
+class named_mutex : public std::mutex {};
+
+template<class... NamedMutexes>
+class mutex_order;
 
 template<class... NamedMutexes>
 class mutex_order_slice {
 public:
     template<class... SliceNamedMutexes>
-    mutex_order_slice(mutex_order_slice<SliceNamedMutexes...>);
+    mutex_order_slice(mutex_order_slice<SliceNamedMutexes...>) {
+        static_assert(can_cut_all<mutex_order_slice<NamedMutexes...>, mutex_order_slice<SliceNamedMutexes...>>::value, "Incorrect type");
+    }
 
     template<class Mutex, class Handler>
     auto lock(Mutex &mut, Handler &&handler) {
         std::lock_guard<Mutex> locker(mut);
         return handler(shrinkSlice<Mutex>());
     }
+
 private:
+    template<class...>
+    friend class mutex_order;
+
+    template<class...>
+    friend class mutex_order_slice;
+
+    mutex_order_slice() = default;
+
     // Выкидывает всё перед Elem и сам Elem
+    template<class Elem, class MutexOrderSlice>
+    struct cut;
+
     template<class Elem, class Head, class... Tail>
-    struct cut {
-        using type = typename cut<Elem, Tail...>::type;
+    struct cut<Elem, mutex_order_slice<Head, Tail...>> {
+        using type = typename cut<Elem, mutex_order_slice<Tail...>>::type;
     };
 
     template<class Elem, class... Other>
-    struct cut<Elem, Elem, Other...> {
+    struct cut<Elem, mutex_order_slice<Elem, Other...>> {
         using type = mutex_order_slice<Other...>;
     };
 
     template<class Mutex>
-    typename cut<Mutex, NamedMutexes...>::type shrinkSlice() {
+    typename cut<Mutex, mutex_order_slice<NamedMutexes...>>::type shrinkSlice() {
         return {};
     }
+
+    template<class MutexOrderSlice1, class MutexOrderSlice2>
+    struct can_cut_all;
+
+    template<class MutexOrderSlice, class X, class... T>
+    struct can_cut_all<mutex_order_slice<X, T...>, MutexOrderSlice> {
+        static constexpr bool value = can_cut_all<mutex_order_slice<T...>, typename cut<X, MutexOrderSlice>::type>::value;
+    };
 };
 
 template<>
@@ -72,7 +97,7 @@ using MutexOrder = mutex_order<MapMutex, MapElemMutex>;
 
 class MapWithElems {
 public:
-    int get(int key, mutex_order_slice<MutexOrder, MapMutex> mo_slice) {
+    int get(int key, mutex_order_slice<MapMutex, MapElemMutex> mo_slice) {
         using std::placeholders::_1;
         using std::placeholders::_2;
         return mo_slice.lock(_mut, std::bind(&MapWithElems::getFromMap, this, key, _1));
@@ -88,14 +113,20 @@ private:
     std::map<int, Elem> _map;
 
     // Этот метод лежит в там же классе, что и get. Но на практике это могут быть методы разных объектов.
-    int getFromMap(int key, mutex_order_slice<MutexOrder, MapElemMutex> mo_slice) {
+    int getFromMap(int key, mutex_order_slice<MapElemMutex> mo_slice) {
         auto &elem = _map[key];
-        auto value = mo_slice.lock(elem._mut, [&elem](mutex_order_slice<MutexOrder> mo_slice) -> int {
+        auto value = mo_slice.lock(elem._mut, [&elem](mutex_order_slice<> mo_slice) -> int {
             return elem._value;
         });
         return value;
     }
 };
+
+int main() {
+    MutexOrder order;
+    MapWithElems map;
+    return map.get(1, order.get());
+}
 
 /*
 #include <string>
